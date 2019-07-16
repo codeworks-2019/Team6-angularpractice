cadb26d06ce316d9f3f5e45248fdbbde866c774e
Randy Doan <49592877+DoanRandy109@users.noreply.github.com>
2019-07-16 10:58:09 -0400

    Added AngularPractice Ex.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Get the elements we want to bind
+var inputElement = document.getElementById('dataToBind');
+var showElement = document.getElementById('showBoundData');
+
+// Add an event listener to listen for keyup event
+inputElement.addEventListener('keyup', function() {
+  showElement.innerHTML = inputElement.value.toString();
+});
+
+
+//Everything above is from the Angular Practice Example
+
+
 
